d42cd0ca87da5937b6f371953a48071a0bd21415
Edson Mendieta <emendietaj@gmail.com>
2016-10-04 11:30:07 -0700

    added exercise solution

--- IntermediateAlgorithmScripting.js
+++ IntermediateAlgorithmScripting.js
@@ -33,3 +33,40 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+
+
+
+// Compares two arrays and returns a new array with items only found in one of the two given arrays, but not both. 
+// In other words, returns the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  var joinedArr = arr1.concat(arr2);
+  var match;
+
+  for (i = 0; i < joinedArr.length; i++) {
+    
+    match = 0;
+    
+    var testNum = joinedArr[i];
+    
+    for (z = 0; z < joinedArr.length; z++) {
+      
+      if (testNum == joinedArr[z]) {
+        
+        match += 1;
+      }
+    }
+    
+    if (match == 1) {
+      
+      newArr.push(joinedArr[i]);
+    }
+    
+  }
+  
+  
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
