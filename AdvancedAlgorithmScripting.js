f2931fc089ba40b1cdff07554a4f076a22542967
Edson Mendieta <emendietaj@gmail.com>
2016-11-22 14:28:23 -0800

    added in-progress solution

--- AdvancedAlgorithmScripting.js
+++ AdvancedAlgorithmScripting.js
@@ -1035,3 +1035,27 @@ bob.getFullName();
 //---------------------------------------------------------------------------------------------------------------------
 
 
+
+//Return a new array that transforms the element's average altitude into their orbital periods.
+//The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+//The values should be rounded to the nearest whole number. The body being orbited is Earth.
+//The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  
+  
+  var time = (2 * Math.PI) * Math.sqrt(Math.pow((earthRadius + arr[0].avgAlt), 3) / GM);
+  
+  
+  
+  return Math.round(time);
+}
+
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+//-----------------------------------------------------------------------------------------------------------------------
+
